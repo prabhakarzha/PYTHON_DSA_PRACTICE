968555bce6c0027854f365fba7c7cc541b8a3f08
prabhakar kumar <70058887+prabhakarzha@users.noreply.github.com>
2021-03-04 11:39:30 +0530

    important code

---
+++ array.py
@@ -0,0 +1,37 @@
+# i =int(input("enter a number"))
+# rev =0
+# while (i>0):
+#     rev =(rev*10)+i%10
+#     i =i//10
+# print("Reverse=",rev)
+
+
+# def all_total(*args):
+#     total =0
+#     for num in args:
+#         total+=num
+#     return total
+# print (all_total(1,2,3,))
+
+# print of multiply of 10 consecutive number in python
+
+# def multiply_nums(*args):
+#     multiply = 1
+#     for i in args:
+#         multiply *= i
+#     return multiply
+# print(multiply_nums(1,2,3,4,5,6,7,8,9,10))
+
+# print of sum of 10 consecutive number
+
+def sum_num(*args):
+    sum =0
+    for i in args:
+        sum += i
+    return sum
+print(sum_num(1,2,3,4,5,6,7,8,9,10))
+
+
+
+
+
