d01f039afcfe17ea7a61105329510c0909eb0b9c
prabhakar kumar <70058887+prabhakarzha@users.noreply.github.com>
2021-05-30 22:07:46 +0530

    oops

---
+++ pythonprogram/nnn.py
@@ -0,0 +1,68 @@
+# for i in range(1,21,2):
+#     print(i ) 
+
+# range(0,10,2)
+# print(range)
+
+
+# n = int(input('Enter Value: '))
+
+# # print('Number from 1 to ' , n)
+
+
+# i=1
+
+
+# while i<=n:
+    
+#     print(i)
+    
+#     i = i+1
+# print(n)  
+
+
+# l = [12,10,23,45,24,10,2]
+ 
+# n =int(input("enter search key" ))
+
+# for i in l:
+#     print(i,n)
+#     if i ==n:
+#         print("found")
+#         break
+ 
+
+
+# class Student:
+    
+#     def __init__(self,name,roll,marks):
+#         self.name =name
+#         self.roll =roll
+#         self.marks =marks
+        
+#     def display(self):
+#         print("Student Name:" , self.name)
+#         print("Student Roll No:" , self.roll)
+#         print("Student Marks:" , self.marks)
+
+
+# S =Student('sonu',11,90)
+# S.display()
+class Student:
+    def __init__(self,m1,m2,m3):
+        self.m1 =m1
+        self.m2 =m2
+        self.m3 =m3
+    
+    def avg(self):
+        return (self.m1+self.m2+self.m3)/3
+
+s = Student(22,33,44)   
+print(s.avg()) 
+    
+
+    
+    
+
+  
+     
