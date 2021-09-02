a579447362a53c37b9f41a772ffabff75e223646
prabhakarzha <kumarprabhakar141@gmail.com>
2021-09-02 22:51:43 +0530

    pair sum & two sum problem

---
+++ Array/pairsum.py
@@ -0,0 +1,47 @@
+def pairsum(A,size,sum):
+    A=sorted(A)
+    left = 0
+    right = size-1
+    count=0
+    while left < right:
+        if (A[left]+A[right] ==sum):
+            count+=1
+            left+=1
+            right-=1
+        elif (A[left] + A[right]>sum):
+            right-=1
+        else:
+            left+=1
+    if count!=0:
+        return count
+    else:
+       return -1
+arr = [8, 7, 2, 5, 3]
+sum = 8
+print(pairsum(arr,len(arr),sum))
+            
+            
+            
+            
+            
+            
+            
+
+
+# def findPair(A, target):
+#     for i in range(len(A) - 1):
+#         for j in range(i + 1, len(A)):
+#             if A[i] + A[j] == target:
+#                 print("Pair found", (A[i], A[j]))
+#                 return
+     
+ 
+ 
+ 
+#     print("Pair not found")
+# if __name__ == '__main__':
+     
+#     A = [8, 7, 2, 5, 3, 1]
+#     target = 10
+ 
+#     findPair(A, target)
