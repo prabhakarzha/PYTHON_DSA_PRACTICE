2861f593b17e0b485b1d2cd3d5e6445f6a4e0449
prabhakarzha <kumarprabhakar141@gmail.com>
2022-04-09 23:20:10 +0530

    linked list problem

---
+++ linkedlist/deletemiddle.py
@@ -0,0 +1,60 @@
+class Node:
+    def __init__(self, data,next):
+        self.data = data
+        self.next = None
+        
+class SingleLinkedList:
+    def __init__(self):
+        self.head = None
+        
+        
+    def addfirst(self,data):
+        newest = Node(data,next)
+        newest.next = self.head
+        self.head = newest
+        
+    def addlast(self,data):
+        newest = Node(data,next)
+        temp=self.head
+        while(temp.next!=None):
+            temp=temp.next
+            
+        temp.next=newest
+    
+    def middlenode(self,data):
+        slow = self.head
+        fast = self.head
+        prevSlow = None
+        
+        while(fast!=None and fast.next!=None):
+            prevSlow = slow
+            slow = slow.next
+            fast = fast.next.next
+        prevSlow.next = slow.next
+        
+        
+            
+      
+    
+    def display(self):
+        temp = self.head
+        while temp:
+            print(temp.data,end = '-->')
+            temp=temp.next
+            
+L= SingleLinkedList()
+L.addfirst(1)
+L.addlast(2)
+L.addlast(3)
+L.addlast(4)
+L.addlast(5)
+L.addlast(6)
+L.addlast(7)
+L.middlenode(4)
+L.display()
+
+
+
+        
+
+        
